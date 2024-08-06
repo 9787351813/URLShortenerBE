247307e8f5e5b25f88822ceb9b3369c43f24417b
Oviya <oviyasraj2001@gmail.com>
2024-08-05 23:18:02 -0700

    initial commit

--- server.js
+++ server.js
@@ -17,13 +17,16 @@ app.use(bodyParser.json());
 
 
 
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://URLShortener:Kavisha123@cluster0.6kv8p2r.mongodb.net/URLShortener')
-.then(() => console.log('Connected to MongoDB'))
-  .catch(err => {
-    console.error('Could not connect to MongoDB', err); // Log connection error
-    process.exit(1);
-  });
+const mongoUri = process.env.MONGODB_URI;
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
+});
 
 // Import auth routes
 const authRoutes = require('./routes/authRoutes'); // Ensure the path is correct
